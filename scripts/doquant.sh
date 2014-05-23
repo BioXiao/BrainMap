574f4e54f3f643e4935d0bc82ffb4d3050e054a3
Loyal Goff <loyalgoff@gmail.com>
2014-05-23 11:54:47 -0400

    Added flowcells 58 and 59. Updated alignment scripts and doquant.sh. Working on putting together reference transcriptome for quantification and differential analysis. Moved tophat tmp directory to regal scratch space in alignreads.sh

--- scripts/doquant.sh
+++ scripts/doquant.sh
@@ -16,32 +16,32 @@ NUM_THREADS=8
 MAX_MEM=8000
 RUN_TIME=600
 
-PROJECT_ROOT = "/n/rinn_data1/seq/lgoff/Projects/BrainMap"
+PROJECT_ROOT="/n/rinn_data1/seq/lgoff/Projects/BrainMap"
 
 ALIGN_ROOT=$PROJECT_ROOT/data/bam
 QUANT_ROOT=$PROJECT_ROOT/data/quants
 
 TMP_DIR=/n/scratch2/rinn_lab/lgoff
 
-BOWTIE_INDEX = $PROJECT_ROOT/data/indexes/mm9_brainmap
+BOWTIE_INDEX=$PROJECT_ROOT/data/indexes/mm9_brainmap
 
-REF_GTF = $PROJECT_ROOT/data/annotation/mm9_ucsc_and_linc_DB_2.0.gtf
+REF_GTF=$PROJECT_ROOT/data/annotation/mm9_ucsc_and_linc_DB_2.0.gtf
 
-LOGBASE = $PROJECT_ROOT/logs
+LOGBASE=$PROJECT_ROOT/logs
 
 #ARGUMENTS
-SAMPLE_NAME = $1
+SAMPLE_NAME=$1
 
-BAMFILE = $2
+BAMFILE=$2
 
 #Main
-LOGDIR = $LOGBASE/alignments/$SAMPLE_NAME
+LOGDIR=$LOGBASE/alignments/$SAMPLE_NAME
 mkdir -p $LOGDIR
 
-OUTDIR = $ALIGN_ROOT/$SAMPLE_NAME
+OUTDIR=$ALIGN_ROOT/$SAMPLE_NAME
 mkdir -p $OUTDIR
 
-echo "#$SAMPLE_NAME"
-sbatch -J $SAMPLE_NAME -t $RUN_TIME --mem-per-cpu=$MAX_MEM -n $NUM_THREADS -p $QUEUE --mail-type=END --wrap=\"cuffquant --no-update-check -p $NUM_THREADS -o $QUANT_ROOT/$SAMPLE_NAME $REF_GTF $BAMFILE >$LOGDIR/$SAMPLE_NAME.quant.out 2>$LOGDIR/$SAMPLE_NAME.quant.err\"
+echo "$SAMPLE_NAME"
+sbatch -J $SAMPLE_NAME -t $RUN_TIME --mem-per-cpu=$MAX_MEM -n $NUM_THREADS -p $QUEUE --mail-type=END --wrap="cuffquant --no-update-check -p $NUM_THREADS -o $QUANT_ROOT/$SAMPLE_NAME $REF_GTF $BAMFILE >$LOGDIR/$SAMPLE_NAME.quant.out 2>$LOGDIR/$SAMPLE_NAME.quant.err"
 
 
