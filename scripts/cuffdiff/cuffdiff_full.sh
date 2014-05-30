a3e09fe6404471a980cde03aa9d327f0fbf78520
Loyal Goff <loyalgoff@gmail.com>
2014-05-30 11:30:31 -0400

    Update to cuffdiff sample sheet and contrasts file for unit_test. Modified cuffdiff_full.sh to run on valor exclusively (was getting bumped on slurm nodes.).  Tweaks to quantScript.sh as well.

--- scripts/cuffdiff/cuffdiff_full.sh
+++ scripts/cuffdiff/cuffdiff_full.sh
@@ -4,33 +4,35 @@
 ###############
 
 #Setup
-PROJECT_ROOT = ""
-DIFF_ROOT = $PROJECT_ROOT/data/diffs
-SAMPLESHEET = "brainMap_sample_sheet.tab"
-CONTRASTFILE = "brainMap_contrasts_full.tab"
+PROJECT_ROOT=/n/rinn_data1/seq/lgoff/Projects/BrainMap
+DIFF_ROOT=$PROJECT_ROOT/data/diffs
+SAMPLESHEET="brainMap_sample_sheet.tab"
+CONTRASTFILE="brainMap_contrasts_full.tab"
 
-REF_GTF = ""
+REF_GTF=$PROJECT_ROOT/data/annotation/BrainMap_ucsc_coding_AND_lnc_DB2.0.gtf
 
-LOGBASE = "../logs"
+LOGBASE=$PROJECT_ROOT/logs
 
 #Slurm
 QUEUE=serial_requeue #unrestricted #or general OR serial_requeue
 NUM_THREADS=8
 MAX_MEM=8000
-RUN_TIME=600
+RUN_TIME=1200
 
 ###########
 #Main
 ###########
 
 #Log directory
-LOGDIR = $LOGBASE/alignments/$SAMPLE_NAME
+LOGDIR=$LOGBASE/diffs/unit_test
 mkdir -p $LOGDIR
 
 #Output director
-OUTDIR = $DIFF_ROOT/full
+OUTDIR=$DIFF_ROOT/unit_test
 mkdir -p $OUTDIR
 
 # Submit cuffdiff
-sbatch -J "BrainMap_full_diff" -t $RUN_TIME --mem-per-cpu=$MAX_MEM -n $NUM_THREADS -p $QUEUE --mail-type=END --wrap=\"cuffdiff -C $CONTRASTFILE -o $OUTDIR --use-sample-sheet $REF_GTF $SAMPLESHEET >$LOGDIR/cuffdiff.out 2>$LOGDIR/cuffdiff.err\""
+#sbatch -J "BrainMap_test_diff" -t $RUN_TIME --mem-per-cpu=$MAX_MEM -n $NUM_THREADS -p $QUEUE --mail-type=END --wrap="cuffdiff -C $CONTRASTFILE -o $OUTDIR --use-sample-sheet $REF_GTF $SAMPLESHEET >$LOGDIR/cuffdiff.out 2>$LOGDIR/cuffdiff.err" >$LOGDIR/slurm.out 2>$LOGDIR/slurm.err
 
+#Choosing to run on valor as opposed to slurm queues due to size and number of comparisons
+cuffdiff -C $CONTRASTFILE -o $OUTDIR --use-sample-sheet $REF_GTF $SAMPLESHEET >$LOGDIR/cuffdiff.out
