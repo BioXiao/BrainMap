a184466dcedb0cd0e087fda6fcdd3d2221b627e8
agroff11 <abbie.groff@gmail.com>
2014-06-18 16:02:07 -0400

    test with overlap, kicked off adult whole thing

--- analysis/overlap_script.R
+++ analysis/overlap_script.R
@@ -45,9 +45,9 @@ getTable<-function(object){
 }
 
 
-setwd("/n/rinn_data1/seq/lgoff/Projects/BrainMap/data/diffs/Peril_vs_WT_Embryonic/")
-cuff<-readCufflinks()
-strain<-"Peril"
+#setwd("/n/rinn_data1/seq/lgoff/Projects/BrainMap/data/diffs/Peril_vs_WT_Embryonic/")
+#cuff<-readCufflinks()
+#strain<-"Peril"
 
 fullTable<-getTable(cuff)
 
@@ -55,22 +55,14 @@ myGene<-fullTable[which(fullTable$gene_short_name==strain),][1,] #any problems w
 chromosome<-myGene$chromosome
 start<-myGene$start-(windowSize/2)
 end<-myGene$end+(windowSize/2)
-#strain_wt_sig<-paste(strain,"_WT_significant", sep="")
-#nSig<-nrow(subset(fullTable,chromosome==chromosome & start>=start &end<=end & strain_wt_sig=="yes"))
 sigGenesRegion<-fullTable[which(fullTable[,40]==chromosome & fullTable[,39]=="yes" & fullTable[,9]>=start & fullTable[,10]<=end),]
 nSig<-nrow(ddply(sigGenesRegion,.(gene_name),head,n=1))
-#nSig<-nrow(fullTable[which(fullTable[,40]==chromosome & fullTable[,39]=="yes" & fullTable[,9]>=start & fullTable[,10]<=end),])
 
 score<-0
 signeighbors<-data.frame(rep(NULL,nIter))
 
 for (i in 1:nIter){
   write(i,stderr())
-#  nSigIter<-nrow(subset(fullTable,chromosome==seqnames(myRandom[i])@values & start>=start(myRandom[i])-(windowSize/2) & 
-# end<=end(myRandom[i])+(windowSize/2) & strain_wt_sig=="yes"))
-#  nSigIter<-nrow(fullTable[which(fullTable[,40]==seqnames(myRandom[i])@values & fullTable[,39]=="yes" & fullTable[,9]>=start(myRandom[i])-(windowSize/2) & 
-# fullTable[,10]<=end(myRandom[i])+(windowSize/2)),])
-####NROWS IS NOT GENES, and may not even be isoforms! 
   sigGenesRegion<-fullTable[which(fullTable[,40]==seqnames(myRandom[i])@values & fullTable[,39]=="yes" & fullTable[,9]>=start(myRandom[i])-(windowSize/2) & fullTable[,10]<=end(myRandom[i])+(windowSize/2)),]
   nSigIter<-nrow(ddply(sigGenesRegion,.(gene_name),head,n=1))
   write(nSigIter,stderr())
@@ -78,10 +70,8 @@ for (i in 1:nIter){
   signeighbors[1,i]<-nSigIter
 }
 
-score/nIter
-
 ttest<-t.test(signeighbors,mu=nSig-1)
-ttest$p.value
+pval_for_region<-ttest$p.value
 
 #first 	question, region enriched over genomic background for cis significance? (pvalue)
 
@@ -93,50 +83,5 @@ genesInRegion$start<-myGene$start-genesInRegion$start
 colnames(genesInRegion)[39]<-"sig"
 colnames(genesInRegion)[35]<-"log2foldchange"
 colnames(genesInRegion)[36]<-"test_stat"
-
-library(plyr)
 data<-ddply(genesInRegion,.(gene_id),head,n=1)
 ggplot(data,aes(start,test_stat,color=sig))+geom_point()+scale_color_manual(values=c("black", "red"))+coord_cartesian(xlim=c(-windowSize/2, windowSize/2))+labs(title=strain)
-
-#data<-cbind(genesInRegion$gene_id, genesInRegion$gene_name, genesInRegion$log2foldchange, genesInRegion$sig, genesInRegion$start, genesInRegion$end)
-#data<-as.data.frame(data)
-#colnames(data)<-c("gene_id","gene_name","log2foldchange","sig","start","end")
-#ddply(data, .(gene_id), head, n = 1) 
-#transform start/stop to center around gene of interest??? (so 0 is our gene?)
-#qplot(start,test_stat,data=data,color=sig)
-#ggplot(data,aes(start,test_stat,color=sig))+geom_point()+scale_fill_manual(values=c("black", "red"))
-ggplot(data,aes(start,test_stat,color=sig))+geom_point()+scale_color_manual(values=c("black", "red"))+coord_cartesian(xlim=c(-windowSize/2, windowSize/2))+labs(title=strain)
-
-
-
-
-
-
-
-
-#P-values (H0=number of significant genes in window is different from that observed for 1000 random intervals)
-# linc-Foxf1a   p<0.087 (1 signficant neighboring gene)
-# linc-Sox2             p<1.0 (0 significant neighboring gene)
-# linc-Brn1b    p<0.225 (4 significant neighboring genes)
-
-
-
-#Simple Image
-#xacis: window size (-100k, +100k; tss @0)
-#yaxis: test stat
-#gene in position across region 
-#colored by yes/no; red/black 
-
-
-
-
-#Draw GeneTrack  
-
-library(Gviz)
-#Granges
-#must read gtf in w cuff 
-cuff<-readCufflinks(gtfFile='/n/rinn_data1/users/agroff/annotation/mm9/ucsc_no_noncoding_AND_lincdb2.gtf',genome='mm9')#will be mm10!
-myID<-"Pde8b"
-myGene<-getGene(cuff,myID)
-genetrack <-makeGeneRegionTrack(myGene)
-plotTracks(genetrack)
