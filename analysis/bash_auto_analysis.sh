9936c0515c7cae1d0eb65d32444bf36e6fab2aaf
agroff11 <abbie.groff@gmail.com>
2014-07-01 18:06:45 -0400

    gsea zscore investigation script added

--- analysis/bash_auto_analysis.sh
+++ analysis/bash_auto_analysis.sh
@@ -1,8 +1,8 @@
 
 
-#for i in {1..26};do
-#	sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript.R $i"
-#done
+for i in {1..26};do
+	sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript.R $i"
+done
 
 #sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript.R 13"
 
@@ -11,9 +11,12 @@
 #sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript.R 23"
 #sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript.R 24"
 
+
+
+####GENE TRACKING ISSUES####
 #linc-cox2 15 and 16
-sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript_cummerbund_genetracking.R 15"
-sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript_cummerbund_genetracking.R 16"
+#sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript_cummerbund_genetracking.R 15"
+#sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript_cummerbund_genetracking.R 16"
 
 #haunt 7
-sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript_cummerbund_genetracking.R 7"
+#sbatch -J R_auto -t 600 --mem-per-cpu=2000 -n 8 -p general --wrap="Rscript runScript_cummerbund_genetracking.R 7"
